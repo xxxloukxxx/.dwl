17edf6a2be7a15f103d1a53a840e92851aa81838
Cedric <cedric.todaro@gmail.com>
2026-05-27 04:55:59 +0000

    Push (2)

--- .dev/dwl/config.h
+++ .dev/dwl/config.h
@@ -10,7 +10,7 @@ static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int showsystray = 1;               /* 0 means no systray */
 static const int showbar = 1;                   /* 0 means no bar */
 static const int topbar = 1;                    /* 0 means bottom bar */
-static const char* fonts[] = { "agave:size=12" };
+static const char* fonts[] = { "agave:size=14" };
 static const float rootcolor[] = COLOR(0x000000ff);
 /* This conforms to the xdg-protocol. Set the alpha to zero to restore the old
  * behavior */
